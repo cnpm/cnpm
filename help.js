ad048427f330b5ee3b95ebe8bb400a75e7347143
dead_horse <dead_horse@qq.com>
2013-12-22 02:49:22 +0800

    fix help info output error, fixed #15

---
+++ help.js
@@ -0,0 +1,53 @@
+/**!
+ * cnpm - help.js
+ *
+ * Copyright(c) cnpmjs.org and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *  dead_horse <dead_horse@qq.com> (http://deadhorse.me)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var config = require('./config');
+
+module.exports = function outputHelp(argv) {
+  var helpInfo = 'Usage: cnpm [option] <command>\n' +
+  'Help: http://cnpmjs.org/help/cnpm\n\n' +
+  '  Extend command\n' +
+  '    web                            open cnpm web (ex.: tnpm web)\n' +
+  '    check [ingoreupdate]           check project dependencies, add ignoreupdate will not check modules\' latest version(ex.: cnpm check, cnpm check -i)\n' +
+  '    doc [moduleName]               open document page (ex.: cnpm doc urllib)\n' +
+  '    sync [moduleName]              sync module from source npm (ex.: cnpm sync urllib)\n' +
+  '    user [username]                open user profile page (ex.: cnpm user fengmk2)\n' +
+  '\n' +
+  '  npm command use --registry=' + (argv.registry || config.cnpmRegistry) + '\n' +
+  '    where <command> is one of:\n' +
+  '    add-user, adduser, apihelp, author, bin, bugs, c, cache,\n' +
+  '    completion, config, ddp, dedupe, deprecate, docs, edit,\n' +
+  '    explore, faq, find, find-dupes, get, help, help-search,\n' +
+  '    home, i, info, init, install, isntall, la, link, list, ll,\n' +
+  '    ln, login, ls, outdated, owner, pack, prefix, prune,\n' +
+  '    publish, r, rb, rebuild, remove, restart, rm, root,\n' +
+  '    run-script, s, se, search, set, show, shrinkwrap, star,\n' +
+  '    start, stop, submodule, tag, test, tst, un, uninstall,\n' +
+  '    unlink, unpublish, unstar, up, update, v, version, view,\n' +
+  '    whoami\n' +
+
+  '      npm <cmd> -h     quick help on <cmd>\n' +
+  '      npm -l           display full usage info\n' +
+  '      npm faq          commonly asked questions\n' +
+  '      npm help <term>  search for help on <term>\n' +
+  '      npm help npm     involved overview\n\n' +
+
+  '      Specify configs in the ini-formatted file:\n' +
+  '          ' + (argv.userconfig || config.userconfig) + '\n' +
+  '      or on the command line via: npm <command> --key value\n' +
+  '      Config info can be viewed via: npm help config';
+  console.log(helpInfo);
+  process.exit(0);
+};
--- parse_argv.js
+++ parse_argv.js
@@ -17,21 +17,38 @@
 
 var program = require('commander');
 var config = require('./config');
-
+var pkg = require('./package.json');
+var help = require('./help');
 var argv;
 module.exports = function () {
   if (argv) {
     return argv;
   }
   argv = program
+    .version(pkg.version, '-v, --version')
     .option('-r, --registry [registry]', 'registry url, default is ' + config.cnpmRegistry)
     .option('--disturl [disturl]', 'dist url for node-gyp, default is ' + config.disturl)
     .option('-w, --registryweb [registryweb]', 'website url, default is ' + config.cnpmHost)
     .option('-c, --cache [cache]', 'cache folder, default is ' + config.cache)
     .option('-u, --userconfig [userconfig]', 'userconfig file, default is ' + config.userconfig)
-    .option('-y, --yes', 'yes all confirm')
-    .parse(process.argv);
-
+    .option('-y, --yes', 'yes all confirm');
+  
+  // custom help message
+  var helpInfo = {};
+  argv.on('registry', function (registry) {
+    helpInfo.registry = registry;
+  });
+  argv.on('userconfig', function (userconfig) {
+    helpInfo.userconfig = userconfig;
+  });
+  // output command help, default options help info will output by default
+  argv.on('--help', function () {
+    help(helpInfo);
+  });
+  argv.parse(process.argv);
+  if (!argv.args.length) {
+    argv.help();
+  }
   argv.registry = argv.registry || config.cnpmRegistry;
   argv.disturl = argv.disturl || config.disturl;
   argv.registryweb = argv.registryweb || config.cnpmHost;
@@ -39,3 +56,4 @@ module.exports = function () {
   argv.userconfig = argv.userconfig || config.userconfig;
   return argv;
 };
+
