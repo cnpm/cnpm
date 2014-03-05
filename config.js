0b87e10d4fa9bc3629a0ea02b32b0d6433747bb9
fengmk2 <fengmk2@gmail.com>
2014-03-05 13:35:16 +0800

    use cnpmjs.org/dist

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ if (process.platform === 'win32') {
 var config = module.exports = {
   cnpmHost: 'http://cnpmjs.org',
   cnpmRegistry: 'http://r.cnpmjs.org',
-  disturl: 'http://dist.u.qiniudn.com', // download dist tarball for node-gyp
+  disturl: 'http://cnpmjs.org/dist', // download dist tarball for node-gyp
   cache: path.join(root, '.npm/.cache/cnpm'),  //cache folder name
   userconfig: path.join(root, '.cnpmrc')
 };
