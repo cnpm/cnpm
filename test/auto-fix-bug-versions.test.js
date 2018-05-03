9543eb6c48b47dea988ef23cf158d4065f1913e7
fengmk2 <fengmk2@gmail.com>
2018-05-04 00:45:58 +0800

    feat: support auto fix bug-versions
    see https://github.com/cnpm/npminstall/blob/68164eac6ea31b61ce683ea5aa23f4c47e9d584e/bin/install.js#L106
    and https://yuque.com/egg/nodejs/npm-package-fastest-bugfix

---
+++ test/auto-fix-bug-versions.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const coffee = require('coffee');
+const rimraf = require('rimraf');
+const cnpm = path.join(__dirname, '..', 'bin', 'cnpm');
+const fixtures = path.join(__dirname, 'fixtures');
+
+describe('test/auto-fix-bug-versions.test.js', () => {
+  const root = path.join(fixtures, 'foo');
+  beforeEach(() => {
+    rimraf.sync(path.join(root, 'node_modules'));
+  });
+  afterEach(() => {
+    rimraf.sync(path.join(root, 'node_modules'));
+  });
+
+  it('should ignore bug version', () => {
+    return coffee.fork(cnpm, [
+        'i',
+        'is-my-json-valid@2.17.0',
+      ], {
+        cwd: root,
+      })
+      .debug()
+      .expect('stderr', /use is-my-json-valid@2\.17\.1 instead/)
+      .expect('code', 0)
+      .end();
+  });
+});
